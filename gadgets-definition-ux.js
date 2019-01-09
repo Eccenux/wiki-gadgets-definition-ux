459f3cbfc77865ef55aa83d1eb735f61fc5d5dd8
Erutuon <Erutuon@fake.wikipedia.org>
2019-01-09 01:57:30 +0000

    -

--- gadgets-definition-ux.js
+++ gadgets-definition-ux.js
@@ -18,7 +18,9 @@ function processGadgetDefinition(innerHTML) {
 			})
 		.replace(/^(\s*)([\w_-]+)\s*/,  // space after gadget name
 			function (wholeMatch, whitespace, gadgetName) {
-				return whitespace + makeWikilink("MediaWiki:Gadget-" + gadgetName, gadgetName);
+				return whitespace
+					+ makeWikilink("MediaWiki:Gadget-" + gadgetName, gadgetName)
+					+ " ";
 			})
 		.replace(/\s*\|\s*/g, " | ") // spaces around pipes
 		.replace(/dependencies\s*=\s*(.+?)(?=\s*[|\]])/g, // spaces around commas in dependencies
