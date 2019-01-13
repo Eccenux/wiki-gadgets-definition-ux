06ea7fd053b7d6ec9d0a023a47a843cda58a5129
Erutuon <Erutuon@fake.wikipedia.org>
2019-01-13 22:11:02 +0000

    add id to list items containing gadgets; link "ext.gadget.<name>" in dependencies to id, link to [[mw:ResourceLoader/Core modules]] for other modules (probably not always correct)

--- gadgets-definition-ux.js
+++ gadgets-definition-ux.js
@@ -20,38 +20,59 @@ function makeWikilink(page, text) {
 	return link.outerHTML;
 }
 
+var gadgetNameRegex = /^(\s*)([\w_-]+)\s*/;
+function getGadgetName(innerHTML) {
+	var match = gadgetNameRegex.exec(innerHTML);
+	return match ? match[2] : null;
+}
+
 function processGadgetDefinition(innerHTML) {
 	return innerHTML
-		.replace(/([\w_-]+\.(?:css|js))/g, // link script names
-			function (scriptFile) {
-				return makeWikilink("MediaWiki:Gadget-" + scriptFile, scriptFile);
-			})
-		.replace(/^(\s*)([\w_-]+)\s*/,  // link gadget name to system message page and add space after it
+		.replace(gadgetNameRegex,  // link gadget name to system message page and add space after it
 			function (wholeMatch, whitespace, gadgetName) {
 				return whitespace
 					+ makeWikilink("MediaWiki:Gadget-" + gadgetName, gadgetName)
 					+ " ";
 			})
+		.replace(/([\w_-]+\.(?:css|js))/g, // link script names
+			function (scriptFile) {
+				return makeWikilink("MediaWiki:Gadget-" + scriptFile, scriptFile);
+			})
 		.replace(/\s*\|\s*/g, " | ") // spaces around pipes
 		.replace(/dependencies\s*=\s*(.+?)(?=\s*[|\]])/g, // spaces around commas in dependencies
 			function (wholeMatch, dependencies) {
-				return "dependencies = " + dependencies.replace(
-					/\s*,\s*/g, ", ");
+				return "dependencies = "
+					+ dependencies.split(/\s*,\s*/g).map(function (dependency) {
+						var match;
+						if ((match = /^ext\.gadget\.(.+)$/.exec(dependency)) !== null) {
+							link.href = mw.util.getUrl("MediaWiki:Gadget-" + match[1]);
+							link.text = dependency;
+							return link.outerHTML;
+						} else {
+							link.href = mw.util.getUrl("mw:ResourceLoader/Core modules#" + dependency);
+							link.text = dependency;
+							return link.outerHTML;
+						}
+						return dependency;
+					}).join(", ");
 			});
 }
 
 var $gadgetsDefinitionContent = $(".page-MediaWiki_Gadgets-definition #mw-content-text");
 
-// Handle gadget definitions in lists.
+// Process gadget definitions in lists.
 $gadgetsDefinitionContent.find("li").each(function (i, element) {
+	gadgetName = getGadgetName(element.innerHTML);
 	element.innerHTML = processGadgetDefinition(element.innerHTML);
+	if (gadgetName)
+		element.id = "Gadget-" + gadgetName;
 });
 
-// Handle gadget definitions in pre tags.
+// Process gadget definitions in pre tags.
 $gadgetsDefinitionContent.find("pre").each(function (i, element) {
 	element.innerHTML = element.innerHTML.replace(/[^\n]+/g, processGadgetDefinition);
 });
 	
-});
+}); // mw.loader.using
 
-})();
+})(); // IIFE
